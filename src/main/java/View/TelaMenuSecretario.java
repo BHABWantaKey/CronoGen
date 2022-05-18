1e83d1b749f85c6c93f642dc220871a4a427c9ef
BHAB <berna.bila@gmail.com>
2022-05-18 09:22:04 +0200

    Menu do docente desenhado

--- src/main/java/View/TelaMenuSecretario.java
+++ src/main/java/View/TelaMenuSecretario.java
@@ -14,14 +14,48 @@ public class TelaMenuSecretario extends JFrame {
     public TelaMenuSecretario(){
 
         setTitle("Menu Principal");
-        setSize(400, 400);
+        setSize(590, 400);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
+        setLocationRelativeTo(null);
+
+
+
+
+        JButton btnDocentes = new JButton("Docentes");
+         JButton btnActividades = new JButton("Actividades");
+         JButton btnCronogramas = new JButton("Cronogramas");
+         JButton btnCriarCronogramas = new JButton("Criar Cronogramas");
+         JButton btnTurmas = new JButton("Turmas");
+         JButton btnRelatorios = new JButton("Relatórios");
+         JButton btnSair = new JButton("Sair");
+         
+         Container container = this.getContentPane();
+         container.setLayout(null);
+
+        btnDocentes.setBounds(50,50,120,50);
+        btnActividades.setBounds(200,50,120,50);
+        btnCronogramas.setBounds(350,50,120,50);
+        btnCriarCronogramas.setBounds(50,200,120,50);
+        btnTurmas.setBounds(200,200,120,50);
+        btnRelatorios.setBounds(350,200,120,50);
+        btnSair.setBounds(470,330,103,30);
+
         
-        
-        
-        
-        }
+
+        container.add(btnDocentes);
+        container.add(btnActividades);
+        container.add(btnCronogramas);
+        container.add(btnCriarCronogramas);
+        container.add(btnTurmas);
+        container.add(btnRelatorios);
+        container.add(btnSair);
+
+}
+
+
+
+         
         
         public static void main(String[] args) {
             
